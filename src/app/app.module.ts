988f699711829bb5bd26c6b98a713070e8d01e2c
amit050505 <verma.amit0407@gmail.com>
2019-12-20 14:41:20 +0530

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports: [ BrowserModule, FormsModule, ButtonsModule,BrowserModule, BrowserAnimationsModule,
+           ],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
